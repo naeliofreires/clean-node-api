bde587da1520ab1926d7f381396805531e50ca7e
Naélio Freires <naeliosistemas@gmail.com>
2025-01-21 10:35:30 -0300

    test: add simple case of tests to SignUpController

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,5 +1,21 @@
 export class SignUpController {
   handle (httpRequest: any): any {
+    const { body } = httpRequest
+
+    if (!body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
+    if (!body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
+    }
+
     return {
       statusCode: 400
     }
--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,5 +14,22 @@ describe('SignUp Controller', () => {
 
     const httpResponse = stu.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const stu = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'name',
+        email: undefined,
+        password: 'password',
+        confirmPassword: 'password'
+      }
+    }
+
+    const httpResponse = stu.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
