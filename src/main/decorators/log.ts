0ff41dd9e69e877acb61aaacc4892e2b617065df
Naélio Freires <naeliosistemas@gmail.com>
2025-02-18 22:55:46 -0300

    feat: add error logging via ILogErrorRepository to LogControllerDecorator

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,17 +1,22 @@
 import { type IController } from '../../presentation/protocols/controllers'
 import { type HttpRequest, type HttpResponse } from '../../presentation/protocols/http'
+import { type ILogErrorRepository } from '../../data/protocols/log-error-repository'
 
 export class LogControllerDecorator implements IController {
   private readonly controller: IController
-  constructor (controller: IController) {
+  private readonly logErrorRepository: ILogErrorRepository
+
+  constructor (controller: IController, logErrorRepository: ILogErrorRepository) {
     this.controller = controller
+    this.logErrorRepository = logErrorRepository
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const response = await this.controller.handle(httpRequest)
 
     if (response.statusCode === 500) {
-      console.log('your log')
+      const stack: string = response.body.stack
+      await this.logErrorRepository.log(stack)
     }
 
     return response
